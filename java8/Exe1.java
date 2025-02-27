f32ed0fb8c1f4a86d7d2fdccb724a899971592a0
Nhokpro2002 <nguyenminhtuan19082002@gmail.com>
2025-02-27 12:19:17 +0700

    java 8 exe 1, 2, 3, 4 , 5, 6

---
+++ java8/Exe1.java
@@ -0,0 +1,30 @@
+package newwavesolution.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Exe1 {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("A");
+        strings.add("b");
+        strings.add("CAsfd");
+        changeStringToLowerCase(strings);
+        System.out.println("\n");
+        changeStringToUpperCase(strings);
+
+    }
+
+    public static void changeStringToUpperCase(List<String> strings) {
+        List<String> stringUpperCase = strings.stream().map(String::toUpperCase).toList();
+        System.out.println(String.join(" ", stringUpperCase));
+
+    }
+
+    public static void changeStringToLowerCase(List<String> strings) {
+        List<String> stringLowerCase = strings.stream().map(String::toLowerCase).toList();
+        System.out.println(String.join(" ", stringLowerCase));
+    }
+}
---
+++ java8/Exe4.java
@@ -0,0 +1,43 @@
+package newwavesolution.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exe4 {
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(-4);
+        integers.add(3);
+        integers.add(1);
+        integers.add(-3);
+        integers.add(-8);
+        findNumber(integers);
+    }
+
+    public static void findNumber(List<Integer> integers) {
+
+        //Object[] array = integers.stream().sorted().toArray();
+        List<Integer> integerList = integers.stream().distinct().sorted().toList();
+
+        if (integerList.isEmpty()) {
+            System.out.println("list empty");
+        }
+        else if (integerList.size() == 1) {
+            System.out.println("list just have 1 element");
+        }
+        else if (integerList.size() == 2) {
+            System.out.println("Second Smallest Number: " + integerList.get(0));
+            System.out.println("Second Largest Number: " + integerList.get(0));
+        }
+        else {
+            System.out.println("Second Smallest Number: " + integerList.get(1));
+            System.out.println("Second Largest Number: " + integerList.get(integerList.size() - 2));
+        }
+
+        /*int secondSmallestNumber = integerList.size() > 1 ? integerList.get(1) : integerList.get(0);
+        int secondLargestNumber = integerList.size() > 1 ? integerList.get(integerList.size() - 2) : integerList.get(0);
+        System.out.println("Second Smallest Number: " + secondSmallestNumber);
+        System.out.println("Second Largest Number: " + secondLargestNumber);*/
+    }
+}
