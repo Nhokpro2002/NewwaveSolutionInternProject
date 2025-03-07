dcfa860e3eb15e04a6ec9fa1becae4df0764ff26
Nhokpro2002 <nguyenminhtuan19082002@gmail.com>
2025-03-07 09:28:38 +0700

    update exe 2

--- SpringExercise/Exercise1/config/AppConfig.java
+++ SpringExercise/Exercise1/config/AppConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.newwave.webbackendlaboratory") // ✅ Quét toàn bộ package này
+@ComponentScan(basePackages = "SpringExercise.Exercise1.service")
 public class AppConfig {
 }
